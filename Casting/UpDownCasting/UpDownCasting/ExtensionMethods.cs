40e6c9bf6f74b1710faf4aaab997fa82c0270ae0
MahammedJafarzade <100687472+MahammedJafarzade@users.noreply.github.com>
2022-04-05 15:03:14 +0400

    Add files via upload

---
+++ Casting/UpDownCasting/UpDownCasting/ExtensionMethods.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpDownCasting
+{
+    public static class ExtensionMethods
+    {
+        public static bool CustomContains(this string word,string soz)
+        {
+            bool check = false;
+            if (word.Contains(soz))
+            {
+                check = true;
+                Console.WriteLine(check);
+                return check;
+
+            }
+            else
+            {
+                Console.WriteLine(check);
+                return check;
+
+            }
+
+        }
+        public static bool CustomContains(this string word, char letter)
+        {
+            bool check = false;
+            foreach (var item in word)
+            {
+                if (item == letter)
+                {
+                    check = true;
+                    Console.WriteLine(check);
+                    return check;
+
+                }
+            }
+            Console.WriteLine(check);
+            return check;
+        }
+       public static bool IsPrime(this int number)
+        {
+            bool check = false;
+            int counter = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    counter++;
+                }
+            }
+            if (counter == 2)
+            {
+                check = true;
+                Console.WriteLine(check);
+                return check;
+
+            }
+            else
+            {
+                Console.WriteLine(check);
+                return check;
+
+            }
+        }
+    }
+}
+
+               
+            
---
+++ Casting/UpDownCasting/UpDownCasting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UpDownCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Maqa";
+            word.CustomContains('a');
+            int num = 65;
+            num.IsPrime();
+        }
+    }
+}
